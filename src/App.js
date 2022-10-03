5e7c7fafd6dbe5aa8de53b22ba4d1f16f2e84a7c
Carlos Rangel <charlesleimaginant@gmail.com>
2022-10-03 00:02:24 -0500

    Using local storage in useEffect to store the value from the searcbox.

--- src/App.js
+++ src/App.js
@@ -2,55 +2,41 @@ import * as React from "react";
 
 
 
-const List = (props) => {
-  console.log('List renders');
-  return(
+const List = ({ list }) => (
     <ul>
-        {props.list.map((item) => (
+        {list.map((item) => (
           <Item key={item.objectID} item={item} />
         ))}
       </ul>
-  )
-}
+);
 
-const Item = (props) => (
+const Item = ({ item }) => (
   <li>
     <span>
-      <a href={props.item.url}>{props.item.title}</a>
+      <a href={item.url}>{item.title}</a>
     </span>
-    <span>{props.item.author}</span>
-    <span>{props.item.num_comments}</span>
-    <span>{props.item.points}</span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span> {item.points}</span>
   </li>
 );
 
-const Search = (props) => {
-  // perform a task in between
-  const [searchTerm, setSearchTerm] = React.useState('');
-  
-  const handleChange = (event) => {
-    // synthetic event
-    console.log(event);
-    // value of target (here: element)
-    //console.log(event.target.value);
-    setSearchTerm(event.target.value);
-    props.onSearch(event);
-  };
-
-  return (
+const Search = ({ search, onSearch }) => (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-
-      <p>
-        Searching for <strong>{searchTerm}</strong>
-      </p>
+      <input 
+        id="search" 
+        type="text" 
+        value={search} 
+        onChange={onSearch} 
+      />
     </div>
-  )
-}
+);
 
 const App = () => {
-  console.log('App renders');
+  const [searchTerm, setSearchTerm] = React.useState(
+    localStorage.getItem('search') || 'React'
+  );
 
   const stories = [
     {
@@ -71,21 +57,27 @@ const App = () => {
     },
   ];
 
-  const handleSearch = (event) => {
-    // D
-    console.log(event.target.value);
-  };
+  React.useEffect(() => {
+    localStorage.setItem('search', searchTerm);
+  }, [searchTerm]);
 
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+  
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search onSearch={handleSearch} />
+      <Search search={searchTerm} onSearch={handleSearch} />
 
       <hr />
 
-      <List list={stories}/>
+      <List list={searchedStories}/>
 
     </div>
   );
